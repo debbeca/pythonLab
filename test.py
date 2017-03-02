183f8c66c18c9eed9ff8780f3d61ea1d086fd94d
debbeca <debbech@gmail.com>
2017-03-02 17:59:39 +0100

    trying sklearn

--- test.py
+++ test.py
@@ -1,13 +1,21 @@
 import pandas as pd
 import numpy as np
 import matplotlib
-
+from sklearn import tree
+from sklearn.preprocessing import LabelEncoder
 
 df = pd.read_csv('data.csv')
 
 df.head()
 df.describe()
-df.plot()
+X = f.ix[:,0:5];
 
 
+le = LabelEncoder()
 
+Y = f.ix[:,5];
+Y_encoded = le.fit_transform(Y)
+y_train = le.fit_transform(X['CI'][:-1])
+
+model = tree.DecisionTreeClassifier(criterion='gini')
+model.fit(X_encoded, y_train)
